eebef8e61e4a90705945c5ff0cdad1e5f991823f
dippelbiz <dippel.biz@gmail.com>
2026-02-05 16:31:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import telebot
 from flask import Flask, request
 
 # ====== НАСТРОЙКИ ======
-TOKEN = os.environ.get('BOT_TOKEN', "8513392038:AAEoKqOuaLIbNc5rcR7QT09VjQeGOpXtHiw")
+TOKEN = os.environ.get('BOT_TOKEN', "8513392038:AAEupfJ198a3AtNinoAsA2h2mmtFIDLOoqk")
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
@@ -161,6 +161,7 @@ if __name__ == '__main__':
     # Запускаем сервер
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
 
 
 
