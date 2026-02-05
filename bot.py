c3df5b592a483489ba7bbb0169e7cff37d59f16e
dippelbiz <dippel.biz@gmail.com>
2026-02-05 12:15:59 +0700

    Refactor bot.py with updated token and structure

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import telebot
 from flask import Flask, request
 
 # ====== НАСТРОЙКИ ======
-TOKEN = os.environ.get('BOT_TOKEN', "8474871816:AAFea3Z--PHfRZOOOKoi-tPE6qHIyQ0iEBE")
+TOKEN = os.environ.get('BOT_TOKEN', "8513392038:AAEoKqOuaLIbNc5rcR7QT09VjQeGOpXtHiw")
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
@@ -161,4 +161,5 @@ if __name__ == '__main__':
     # Запускаем сервер
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
 
